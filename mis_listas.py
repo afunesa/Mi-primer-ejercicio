32d9d19a2beb39a69783976d2810679629ae7755
antonia.funes <Motril16>
2021-02-23 19:30:09 +0100

    add mis listas

---
+++ mis_listas.py
@@ -0,0 +1,13 @@
+#listas
+#todo son objetos y metodos
+
+mi_primera_lista=['platanos', 'manzanas', 'fresas']
+print(mi_primera_lista)
+print(mi_primera_lista.index('manzanas'))
+mi_primera_lista.append('naranjas')
+mi_primera_lista.append('peras')
+print(mi_primera_lista)
+mi_primera_lista.sort()
+print(mi_primera_lista)
+mi_primera_lista.sort(reverse=True)
+print(mi_primera_lista)
