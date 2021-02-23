9eb019b4f8e527cf020d840c73d92d2bf267a69f
antonia.funes <Motril16>
2021-02-23 19:14:44 +0100

    add lists

---
+++ basico.py
@@ -0,0 +1,21 @@
+# Primeros pasos cn datos basicos
+saludo= "Hola a todos!"
+numero_uno = 1
+numero_dos_decimales = 1.25E3
+primera_letra = "A"
+print(saludo)
+print("Empezamos el curso" + " " + primera_letra)
+
+print(bool(9))
+print(bool(0))
+print(bool("blabla"))
+
+print(numero_uno + numero_dos_decimales)
+
+primera_letra = "aa"
+print("Empezamos el curso" + " " + primera_letra)
+
+print(saludo * 2)
+
+
+
